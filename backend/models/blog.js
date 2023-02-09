77bd8f16bce85f2d9e764df49bdac2556cddd46c
cragtok <lidfun@protonmail.com>
2023-02-10 05:05:53 +0600

    feat: Add timestamp to blog model

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,50 +1,53 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        unique: [true, "Blog title is already taken"],
-        required: [true, "Blog title required"],
-        minlength: [5, "Blog title must be at least 5 characters long"],
-        maxlength: [35, "Blog title must be shorter than 36 characters"],
-    },
-    author: {
-        type: String,
-        required: [true, "Author name required"],
-        minlength: [3, "Author name must be at least 3 characters long"],
-        maxlength: [20, "Author name must be shorter than 21 characters"],
-    },
-    url: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                const pattern = new RegExp(
-                    "^((ft|htt)ps?:\\/\\/)?" + // protocol
-                        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name and extension
-                        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-                        "(\\:\\d+)?" + // port
-                        "(\\/[-a-z\\d%@_.~+&:]*)*" + // path
-                        "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" + // query string
-                        "(\\#[-a-z\\d_]*)?$",
-                    "i"
-                ); // fragment locator
-                return pattern.test(v);
-            },
-            message: props => `${props.value} is not a valid url.`,
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            unique: [true, "Blog title is already taken"],
+            required: [true, "Blog title required"],
+            minlength: [5, "Blog title must be at least 5 characters long"],
+            maxlength: [35, "Blog title must be shorter than 36 characters"],
         },
-        required: [true, "Blog url required."],
+        author: {
+            type: String,
+            required: [true, "Author name required"],
+            minlength: [3, "Author name must be at least 3 characters long"],
+            maxlength: [20, "Author name must be shorter than 21 characters"],
+        },
+        url: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    const pattern = new RegExp(
+                        "^((ft|htt)ps?:\\/\\/)?" + // protocol
+                            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name and extension
+                            "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+                            "(\\:\\d+)?" + // port
+                            "(\\/[-a-z\\d%@_.~+&:]*)*" + // path
+                            "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" + // query string
+                            "(\\#[-a-z\\d_]*)?$",
+                        "i"
+                    ); // fragment locator
+                    return pattern.test(v);
+                },
+                message: props => `${props.value} is not a valid url.`,
+            },
+            required: [true, "Blog url required."],
+        },
+        likes: {
+            type: Number,
+            default: 0,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        comments: [{ type: "String" }],
+        userLikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     },
-    likes: {
-        type: Number,
-        default: 0,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    comments: [{ type: "String" }],
-    userLikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
+    { timestamps: true }
+);
 
 blogSchema.set("toJSON", {
     transform: (document, returnedObject) => {
