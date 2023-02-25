068c917c07b9a661070dea5e300be1a9f5312ba8
cragtok <lidfun@protonmail.com>
2023-02-25 15:16:59 +0600

    refactor: Add separate functions for rendering sorted and filtered fields

--- frontend/src/components/BlogLink.js
+++ frontend/src/components/BlogLink.js
@@ -10,8 +10,6 @@ const blogStyle = {
 
 const BlogLink = ({ blog, sortedField, filteredFields }) => {
     const { id, title, author, createdAt, comments, likes, url } = blog;
-    const blogDate = new Date(createdAt).toDateString().slice(4);
-    const blogTime = new Date(createdAt).toLocaleTimeString();
 
     const selectTextColor = field => {
         if (filteredFields.includes(field)) {
@@ -21,36 +19,64 @@ const BlogLink = ({ blog, sortedField, filteredFields }) => {
         return "";
     };
 
+    const selectFilteredText = filteredField => {
+        /*eslint-disable indent */
+        let filteredText;
+        switch (filteredField) {
+            case "numComments":
+                filteredText = `Comments: ${comments.length}`;
+                break;
+            case "numLikes":
+                filteredText = `Likes: ${likes}`;
+                break;
+            case "url":
+                filteredText = `URL: ${url}`;
+                break;
+            case "likedBlogs":
+                filteredText = "Liked";
+                break;
+            case "commentedBlogs":
+                filteredText = "Commented";
+                break;
+            default:
+                filteredText = "";
+        }
+        return filteredText;
+    };
+
     const renderFilteredFields = () => {
         return filteredFields.map(filteredField => {
-            let filteredText;
-            /*eslint-disable indent */
-            switch (filteredField) {
-                case "numComments":
-                    filteredText = `Comments: ${comments.length}`;
-                    break;
-                case "numLikes":
-                    filteredText = `Likes: ${likes}`;
-                    break;
-                case "url":
-                    filteredText = `URL: ${url}`;
-                    break;
-                case "likedBlogs":
-                    filteredText = "Liked";
-                    break;
-                case "commentedBlogs":
-                    filteredText = "Commented";
-                    break;
-                default:
-                    return null;
-            }
             return (
                 <p className="has-text-info" key={crypto.randomUUID()}>
-                    {filteredText}
+                    {selectFilteredText(filteredField)}
                 </p>
             );
         });
     };
+
+    const selectSortedText = sortedField => {
+        let sortedText = "";
+        switch (sortedField) {
+            case "likes":
+                sortedText = `Likes: ${likes}`;
+                break;
+            case "comments":
+                sortedText = `Comments: ${comments.length}`;
+                break;
+            default:
+                sortedText = "";
+        }
+        return sortedText;
+    };
+
+    const renderSortedField = () => {
+        return (
+            <p className="has-text-success">{selectSortedText(sortedField)}</p>
+        );
+    };
+
+    const blogDate = new Date(createdAt).toDateString().slice(4);
+    const blogTime = new Date(createdAt).toLocaleTimeString();
 
     return (
         <Link to={`/blogs/${id}`}>
@@ -64,15 +90,8 @@ const BlogLink = ({ blog, sortedField, filteredFields }) => {
                 </p>
                 <span className={selectTextColor("title")}>{title}</span> by{" "}
                 <span className={selectTextColor("author")}>{author}</span>
-                {sortedField === "comments" && (
-                    <p className="has-text-success">
-                        Comments: {comments.length}
-                    </p>
-                )}
-                {sortedField === "likes" && (
-                    <p className="has-text-info">Likes: {likes}</p>
-                )}
-                {filteredFields.length > 0 && <>{renderFilteredFields()}</>}
+                {sortedField && renderSortedField(sortedField)}
+                {filteredFields.length > 0 && renderFilteredFields()}
             </div>
         </Link>
     );
