5fbe7df9fb3ae6355cad90b006ed3ddbf7288b3e
cragtok <lidfun@protonmail.com>
2023-02-25 14:57:57 +0600

    feat: Render separate colors for filtered and sorted fields

--- frontend/src/components/BlogLink.js
+++ frontend/src/components/BlogLink.js
@@ -13,10 +13,13 @@ const BlogLink = ({ blog, sortedField, filteredFields }) => {
     const blogDate = new Date(createdAt).toDateString().slice(4);
     const blogTime = new Date(createdAt).toLocaleTimeString();
 
-    const setFieldColor = field =>
-        sortedField === field || filteredFields.includes(field)
-            ? "has-text-success"
-            : "";
+    const selectTextColor = field => {
+        if (filteredFields.includes(field)) {
+            return "has-text-info";
+        }
+        if (sortedField === field) return "has-text-success";
+        return "";
+    };
 
     const renderFilteredFields = () => {
         return filteredFields.map(filteredField => {
@@ -42,7 +45,7 @@ const BlogLink = ({ blog, sortedField, filteredFields }) => {
                     return null;
             }
             return (
-                <p className="has-text-success" key={crypto.randomUUID()}>
+                <p className="has-text-info" key={crypto.randomUUID()}>
                     {filteredText}
                 </p>
             );
@@ -54,20 +57,20 @@ const BlogLink = ({ blog, sortedField, filteredFields }) => {
             <div style={blogStyle}>
                 <p
                     className={`subtitle-6 is-italic ${
-                        setFieldColor("date") || setFieldColor("createdAt")
+                        selectTextColor("date") || selectTextColor("createdAt")
                     }`}
                 >
                     {blogDate} {blogTime}
                 </p>
-                <span className={setFieldColor("title")}>{title}</span> by{" "}
-                <span className={setFieldColor("author")}>{author}</span>
+                <span className={selectTextColor("title")}>{title}</span> by{" "}
+                <span className={selectTextColor("author")}>{author}</span>
                 {sortedField === "comments" && (
                     <p className="has-text-success">
                         Comments: {comments.length}
                     </p>
                 )}
                 {sortedField === "likes" && (
-                    <p className="has-text-success">Likes: {likes}</p>
+                    <p className="has-text-info">Likes: {likes}</p>
                 )}
                 {filteredFields.length > 0 && <>{renderFilteredFields()}</>}
             </div>
