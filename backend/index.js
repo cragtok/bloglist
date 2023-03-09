467d92bce79f4f154704d61aace2365e602941ce
cragtok <lidfun@protonmail.com>
2023-03-10 04:01:28 +0600

    fix: Add alternate port in backend index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const logger = require("./utils/logger");
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
     logger.info(
-        `Server running on port ${config.PORT} in ${process.env.NODE_ENV} mode`
+        `Server running on port ${config.PORT || 3003} in ${
+            process.env.NODE_ENV
+        } mode`
     );
 });
