f9520d1fc01cb412deaf4f6f7dae6bb47c315ab3
cragtok <lidfun@protonmail.com>
2023-03-10 04:16:13 +0600

    fix: Establish mongoose connection before listening on server

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,19 +7,6 @@ const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
 
 const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
-
-logger.info("connecting to MongoDB");
-
-mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info("connected to MongoDB");
-    })
-    .catch(error => {
-        logger.error("error connecting to MongoDB:", error.message);
-    });
 
 app.use(cors());
 
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,22 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
+const mongoose = require("mongoose");
 const server = http.createServer(app);
 const port = config.PORT || 3003;
-server.listen(port, () => {
-    logger.info(
-        `Server running on port ${port} in ${process.env.NODE_ENV} mode`
-    );
-});
+
+logger.info("connecting to MongoDB");
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("connected to MongoDB");
+        server.listen(port, () => {
+            logger.info(
+                `Server running on port ${port} in ${process.env.NODE_ENV} mode`
+            );
+        });
+    })
+    .catch(error => {
+        logger.error("error connecting to MongoDB:", error.message);
+    });
