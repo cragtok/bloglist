0141df42f2561a746cfb3e83a52f2e64905b4488
cragtok <lidfun@protonmail.com>
2023-03-10 04:45:25 +0600

    refactor: Refactor initial mongoDB connection and server listening

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,21 @@ const port = config.PORT || 3003;
 
 logger.info("connecting to MongoDB");
 
-mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info("connected to MongoDB");
-        server.listen(port, () => {
-            logger.info(
-                `Server running on port ${port} in ${process.env.NODE_ENV} mode`
-            );
-        });
-    })
-    .catch(error => {
-        logger.error("error connecting to MongoDB:", error.message);
+const connectDB = async () => {
+    try {
+        mongoose.set("strictQuery", true);
+        const connection = await mongoose.connect(config.MONGODB_URI);
+        logger.info(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        logger.error(error);
+        process.exit(1);
+    }
+};
+
+connectDB().then(() => {
+    server.listen(port, () => {
+        logger.info(
+            `Server running on port ${port} in ${process.env.NODE_ENV} mode`
+        );
     });
+});
